1cd0ed5e20db8ed4282ed87d3daddef3273a159d
Sergey Kostenko <sergejkostenko@rambler.ru>
2022-01-08 12:26:12 +0300

    new file

---
+++ kop.java
@@ -0,0 +1,8 @@
+package task_3_1;
+
+public class kop {
+    public static void main(String[] args) {
+        System.out.println((896/100)%10);//896/100= 8 89%10 =9
+        System.out.println(8%10);
+    }
+}
